e37ee7ee254de1a086b4520776349433e6199a85
franky <poi861111@gmail.com>
2020-03-08 15:32:16 +0800

    Create uva12019.cpp

---
+++ Cpp_ex/CPE/uva12019.cpp
@@ -0,0 +1,377 @@
+/*
+    ------------- 2020/03/08 --------------
+    1. ³q¹L 
+*/
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int a=0,b=0,c=0,d=0;
+	scanf("%d\n",&c);
+	while(c--)
+	{
+		cin >> a >> b;
+		if (a == 1)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Saturday" << endl;
+					break;
+				case 2:
+					cout << "Sunday" << endl;
+					break;
+				case 3:
+					cout << "Monday" << endl;
+					break;
+				case 4:
+					cout << "Tuseday" << endl;
+					break;
+				case 5:
+					cout << "Wednesday" << endl;
+					break;
+				case 6:
+					cout << "Thursday" << endl;
+					break;
+				case 0:
+					cout << "Friday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+		if (a == 2)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Tuesday" << endl;
+					break;
+				case 2:
+					cout << "Wednesday" << endl;
+					break;
+				case 3:
+					cout << "Thursday" << endl;
+					break;
+				case 4:
+					cout << "Friday" << endl;
+					break;
+				case 5:
+					cout << "Saturday" << endl;
+					break;
+				case 6:
+					cout << "Sunday" << endl;
+					break;
+				case 0:
+					cout << "Monday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+		if (a == 3)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Tuesday" << endl;
+					break;
+				case 2:
+					cout << "Wednesday" << endl;
+					break;
+				case 3:
+					cout << "Thursday" << endl;
+					break;
+				case 4:
+					cout << "Friday" << endl;
+					break;
+				case 5:
+					cout << "Saturday" << endl;
+					break;
+				case 6:
+					cout << "Sunday" << endl;
+					break;
+				case 0:
+					cout << "Monday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+		if (a == 4)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Friday" << endl;
+					break;
+				case 2:
+					cout << "Saturday" << endl;
+					break;
+				case 3:
+					cout << "Sunday" << endl;
+					break;
+				case 4:
+					cout << "Monday" << endl;
+					break;
+				case 5:
+					cout << "Tuesday" << endl;
+					break;
+				case 6:
+					cout << "Wednesday" << endl;
+					break;
+				case 0:
+					cout << "Thursday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+		if (a == 5)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Sunday" << endl;
+					break;
+				case 2:
+					cout << "Monday" << endl;
+					break;
+				case 3:
+					cout << "Tuesday" << endl;
+					break;
+				case 4:
+					cout << "Wednesday" << endl;
+					break;
+				case 5:
+					cout << "Thursday" << endl;
+					break;
+				case 6:
+					cout << "Friday" << endl;
+					break;
+				case 0:
+					cout << "Saturday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+		if (a == 6)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Wednesday" << endl;
+					break;
+				case 2:
+					cout << "Thursday" << endl;
+					break;
+				case 3:
+					cout << "Friday" << endl;
+					break;
+				case 4:
+					cout << "Saturday" << endl;
+					break;
+				case 5:
+					cout << "Sunday" << endl;
+					break;
+				case 6:
+					cout << "Monday" << endl;
+					break;
+				case 0:
+					cout << "Tuesday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+		if (a == 7)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Friday" << endl;
+					break;
+				case 2:
+					cout << "Saturday" << endl;
+					break;
+				case 3:
+					cout << "Sunday" << endl;
+					break;
+				case 4:
+					cout << "Monday" << endl;
+					break;
+				case 5:
+					cout << "Tuesday" << endl;
+					break;
+				case 6:
+					cout << "Wednesday" << endl;
+					break;
+				case 0:
+					cout << "Thursday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+		if (a == 8)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Monday" << endl;
+					break;
+				case 2:
+					cout << "Tuesday" << endl;
+					break;
+				case 3:
+					cout << "Wednesday" << endl;
+					break;
+				case 4:
+					cout << "Thursday" << endl;
+					break;
+				case 5:
+					cout << "Friday" << endl;
+					break;
+				case 6:
+					cout << "Saturday" << endl;
+					break;
+				case 0:
+					cout << "Sunday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+		if (a == 9)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Thursday" << endl;
+					break;
+				case 2:
+					cout << "Friday" << endl;
+					break;
+				case 3:
+					cout << "Saturday" << endl;
+					break;
+				case 4:
+					cout << "Sunday" << endl;
+					break;
+				case 5:
+					cout << "Monday" << endl;
+					break;
+				case 6:
+					cout << "Tuesday" << endl;
+					break;
+				case 0:
+					cout << "Wednesday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+		if (a == 10)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Saturday" << endl;
+					break;
+				case 2:
+					cout << "Sunday" << endl;
+					break;
+				case 3:
+					cout << "Monday" << endl;
+					break;
+				case 4:
+					cout << "Tuseday" << endl;
+					break;
+				case 5:
+					cout << "Wednesday" << endl;
+					break;
+				case 6:
+					cout << "Thursday" << endl;
+					break;
+				case 0:
+					cout << "Friday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+		if (a == 11)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Tuesday" << endl;
+					break;
+				case 2:
+					cout << "Wednesday" << endl;
+					break;
+				case 3:
+					cout << "Thursday" << endl;
+					break;
+				case 4:
+					cout << "Friday" << endl;
+					break;
+				case 5:
+					cout << "Saturday" << endl;
+					break;
+				case 6:
+					cout << "Sunday" << endl;
+					break;
+				case 0:
+					cout << "Monday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+		if (a == 12)
+		{
+			b = b%7;
+			switch(b)
+			{
+				case 1:
+					cout << "Thursday" << endl;
+					break;
+				case 2:
+					cout << "Friday" << endl;
+					break;
+				case 3:
+					cout << "Saturday" << endl;
+					break;
+				case 4:
+					cout << "Sunday" << endl;
+					break;
+				case 5:
+					cout << "Monday" << endl;
+					break;
+				case 6:
+					cout << "Tuesday" << endl;
+					break;
+				case 0:
+					cout << "Wednesday" << endl;
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
